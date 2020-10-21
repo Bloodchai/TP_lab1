e5cf8bae54da8fe0ba69e66b72cdb6b9bb079734
Bloodchai <artdor99@gmail.com>
2020-10-21 19:32:29 +0400

    'Lab_done'

--- TP_lab1.py
+++ TP_lab1.py
@@ -1,38 +1,6 @@
-import requests
-import json
+import WeatherProvider
 from sqlalchemy import create_engine, Table, Column, String, Float, MetaData
 from sqlalchemy.sql import select
-
-
-class WeatherProvider:
-    def __init__(self, key):
-        self.key = key
-
-    def get_data(self, location, start_date, end_date):
-        url = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx'
-        params = {
-            'q': location,
-            'date': f'{start_date}',
-            'enddate': f'{end_date}',
-            'tp': 24,
-            'format': 'json',
-            'key': self.key,
-        }
-        data = requests.get(url, params).json()
-        with open('myWeatherData.json', 'w') as outfile:
-            json.dump(data, outfile, indent = 4)
-        return [
-            {
-                'date': row['date'][:10],
-                'mint': row['mintempC'],
-                'maxt': row['maxtempC'],
-                'location': 'Volgograd,Russia',
-                'humidity': row['hourly'][0]['humidity']
-            }
-            for row in data['data']['weather']
-        ]
-        return data
-
 
 engine = create_engine('sqlite:///weather.sqlite3')
 metadata = MetaData()
@@ -49,7 +17,7 @@ metadata.create_all(engine)
 
 c = engine.connect()
 
-provider = WeatherProvider('9882fbe5bd364ef1885201031202010')
+provider = WeatherProvider.WeatherProvider('9882fbe5bd364ef1885201031202010')
 c.execute(weather.insert(), provider.get_data('Volgograd, Russia', '2020-09-20', '2020-09-29'))
 
 for row in c.execute(select([weather])):
---
+++ WeatherProvider.py
@@ -0,0 +1,31 @@
+import json
+import requests
+
+class WeatherProvider:
+    def __init__(self, key):
+        self.key = key
+
+    def get_data(self, location, start_date, end_date):
+        url = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx'
+        params = {
+            'q': location,
+            'date': f'{start_date}',
+            'enddate': f'{end_date}',
+            'tp': 24,
+            'format': 'json',
+            'key': self.key,
+        }
+        data = requests.get(url, params).json()
+        with open('myWeatherData.json', 'w') as outfile:
+            json.dump(data, outfile, indent = 4)
+        return [
+            {
+                'date': row['date'][:10],
+                'mint': row['mintempC'],
+                'maxt': row['maxtempC'],
+                'location': 'Volgograd,Russia',
+                'humidity': row['hourly'][0]['humidity']
+            }
+            for row in data['data']['weather']
+        ]
+        return data
